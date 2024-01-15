d77f72ce9648fddf84036151d79a1272d452f4e7
Matt Chandler <mchandler.dev@gmail.com>
2024-01-15 10:09:08 -0800

    allow db connection in rails console

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Flix
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Enable DB connection in console
+    console do
+      ActiveRecord::Base.connection
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
