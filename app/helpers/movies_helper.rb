eef8087f2c24b5f8886bd46deaa0fa7768497cd8
Matt Chandler <mchandler.dev@gmail.com>
2024-01-16 19:22:00 -0800

    helper functions

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+    def format_total_gross(movie)
+        if movie.flop?
+            content_tag(:strong, 'Flop!')
+        else
+            content_tag(:strong, "Watchable")
+        end
+    end
 end
