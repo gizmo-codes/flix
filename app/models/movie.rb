2d73ea5f46a2fe820e01a96fda7b1df0ef51f40f
Matt Chandler <mchandler.dev@gmail.com>
2024-01-16 19:22:22 -0800

    example of 'business logic' that should be done in the model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+    # Will return true or false, since it's a function that ends in a question mark.
+    def flop?
+        total_gross.blank? || total_gross < 50000000
+    end
 end
